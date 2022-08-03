43fcbb705189ffbd172e8140a7b2913591e4aed2
Fernando Mastropietro <femastro@gmail.com>
2022-08-03 09:04:47 -0300

    first commit

--- node/index.js
+++ node/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
-const conexion= mysql.createConnection({
+const conexion = mysql.createConnection({
     host : 'localhost',
     database : 'prueba',
     user : 'root',
